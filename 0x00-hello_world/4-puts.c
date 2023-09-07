607dfa5781e0eb1a56bf85880d700642450ef5d0
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-07 14:15:14 -0700

    4-puts.c fix 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
+#include<stdio.h>
 /**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
-* 
-* Use the function puts
-* You are not allowed to use printf
-* Your program should end with the value 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
 #include <stdio.h>
 
 int main(void)
