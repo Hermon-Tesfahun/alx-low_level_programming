2a15c8d061794995c5467c6fd767dc7430f7fec6
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-11 14:33:44 -0700

    q6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 9.
+ *
+ * Return: returns 0 if the program is Succesful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+}
