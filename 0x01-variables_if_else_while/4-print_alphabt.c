ad30ee79f57d5b8fb02d9d907cfaa5145d44f4ea
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-11 14:27:58 -0700

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except q & e
+ *
+ * Return: returns 0 if Successful
+ */
+int main(void)
+{
+	char alp = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if(alp != 'e' || alp != 'q')
+		{
+			putchar(alp);
+		}
+		alp++;
+	}
+	putchar('\n');
+	return (0);
+}
+
