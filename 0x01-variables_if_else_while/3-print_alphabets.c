b4748ac019265df6a91d25f72d009954ded65bf0
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-11 14:16:15 -0700

    4 try 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,14 @@ int main(void)
 char alp='a';
 	int i;
 
-	for (i = 0; i < 52; i++)
-	{
-	if(i < 26)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp++);
 	}
-	else
-	{
-		alp= 'A';
-		putchar(alp++);
-	}
+	alp='A';
+	for (j = 0; j < 26; j++)
+        {
+       		putchar(alp++);
 	}
 	putchar('\n');
 	return (0);
