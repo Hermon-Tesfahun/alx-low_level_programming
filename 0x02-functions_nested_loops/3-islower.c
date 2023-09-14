cb2fe0340fa3b0736cede1d54812bc836a22ff9f
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-14 14:52:45 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if letter is lowercase
+ * @c: is the letter to be checked
+ * Return: 1 if the letter is lowercase, else 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
