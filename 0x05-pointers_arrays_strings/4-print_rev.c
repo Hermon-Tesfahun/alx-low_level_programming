8348f9b069296990da1faa16e1daa94c4577306b
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-18 14:32:53 -0700

    2,3,4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int stringLength = 0;
+
+	while (*s != '\0')
+	{
+		stringLength++;
+		s++;
+	}	
+}
