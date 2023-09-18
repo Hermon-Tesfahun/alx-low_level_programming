6dae3a1e4f09d97cb8001b93e2af88878fc998be
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-18 14:43:18 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+	count++;
+	for (i = 0; i < count; i++)
+	{
+		count--;
+		reverse = s[i];
+		s[i] = s[count];
+		s[counter] = reverse;
+	}
+}
+
