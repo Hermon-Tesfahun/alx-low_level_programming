ad171f19dbd3084c6a0d4f72f235846b4d6dbc91
Hermon-Tesfahun <hermontesfahun215.com>
2023-09-22 19:32:34 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat -  function that copies a string.
+ * @dest: string to be appended to
+ * @src: string to to be appended
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
