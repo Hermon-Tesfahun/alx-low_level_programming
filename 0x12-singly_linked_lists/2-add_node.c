e22a826b0793399bae2800e6d45f502961e5a97d
Hermon-Tesfahun <hermontesfahun215.com>
2023-10-19 13:23:03 -0700

    2-add_node.c 3rd try

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int size = 0;
-
-	while (str)
-		str = str->next;
-		size++;
-
+	
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
